6499c4c097d1358ae8a7b14f4b5d26355f5763d7
Roman Romanov <romanromanov@MacBook-Air-Roman.local>
2022-06-05 21:15:20 +0300

    Add beta file

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include_once './db_connect.php';
+if (isset($_POST['login'])){
+    $login = $_POST['login'];
+    $pass = md5($_POST['pass']);
+    $rs = $db->query("SELECT * FROM users WHERE login = '$login'")->fetch();
+    if (!empty($rs['login'])){
+        if ($pass == $rs['password']){
+            echo "Успешная авторизация!";
+        } else {
+            //Неверный пароль
+        }
+    } else {
+        //Несуществующий пользователь
+    }
+}
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Авторизация</title>
+</head>
+<body>
+<form method="POST" action="./">
+    <input type="text" name="login">
+    <input type="password" name="pass">
+    <input type="submit" value="Войти">
+</form>
+</body>
+</html>
