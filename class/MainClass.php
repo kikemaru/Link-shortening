1123b55a225b31a480d5ce93e30245d3f389ab19
Roman Romanov <romanromanov@MacBook-Air-Roman.local>
2022-06-06 10:40:07 +0300

    Update class file

--- class/MainClass.php
+++ class/MainClass.php
@@ -8,11 +8,28 @@ class MainClass
     private $db;
     private $idlink;
 
-    public function __construct($user, $id = "empty")
+    public function __construct($user = "empty", $id = "empty")
     {
         $this->user = $user;
         $this->idlink = $id;
         $this->db = new PDO('mysql:host=localhost;dbname=short', 'root', '');
     }
 
+    public function AuthUser($login, $password){
+        $ulogin = $login;
+        $upass = md5($password);
+
+        $auth = $this->db->query("SELECT * FROM users WHERE login = '$ulogin'")->fetch();
+        if (!empty($auth['login'])){
+            if ($upass == $auth['password']){
+                echo "OK";
+            } else {
+                echo "Error pass";
+                //Неверный пароль
+            }
+        } else {
+            echo "Error user";
+            //Несуществующий пользователь
+        }
+    }
 }
