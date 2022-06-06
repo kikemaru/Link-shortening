edb503cf4d8db536dfc65c21d12f5f9f00aa394e
Roman Romanov <romanromanov@MacBook-Air-Roman.local>
2022-06-06 10:31:31 +0300

    Update class file

--- class/MainClass.php
+++ class/MainClass.php
@@ -1,6 +1,18 @@
 <?php
 
+use PDO;
+
 class MainClass
 {
+    private $user;
+    private $db;
+    private $idlink;
+
+    public function __construct($user, $id = "empty")
+    {
+        $this->user = $user;
+        $this->idlink = $id;
+        $this->db = new PDO('mysql:host=localhost;dbname=short', 'root', '');
+    }
 
 }
