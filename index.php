cc1284e47a9b6a5f29b30593a8eb721e7269f21d
Roman Romanov <romanromanov@MacBook-Air-Roman.local>
2022-06-07 06:59:46 +0300

    Update index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_POST['login'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Авторизация</title>
+    <title>Вход</title>
 </head>
 <body>
 <form method="POST" action="./">
